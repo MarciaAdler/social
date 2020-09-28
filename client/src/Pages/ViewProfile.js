bb4133d7784f21b135b0d9d34389d42539eb45b8
Marcia <marciaadler718@gmail.com>
2020-09-28 09:57:29 -0400

    added chat button to user profile pages and updated styling of page

--- client/src/Pages/ViewProfile.js
+++ client/src/Pages/ViewProfile.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import UserProfile from "../components/UserProfile";
+import Chat from "../components/Chat";
+import { useStoreContext } from "../utils/GlobalState";
 
 export default function Profile() {
+  const [state, dispatch] = useStoreContext();
   return (
-    <div className="text-center">
-      <Container className="viewprofile--container">
+    <Container className="home--chat text-center">
+      {state.loggedin === true ? <Chat /> : ""}
+      <Container className="home--container">
         <UserProfile />
       </Container>
-    </div>
+    </Container>
   );
 }
