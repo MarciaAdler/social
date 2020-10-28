38206a9aaab16dde6388a50b9cedfd8f3e239582
Marcia <marciaadler718@gmail.com>
2020-10-28 11:27:01 -0400

    fixed server to try to fix error on live site

--- server.js
+++ server.js
@@ -155,7 +155,7 @@ app.post("/api/grouppostimage", (req, res) => {
 });
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
